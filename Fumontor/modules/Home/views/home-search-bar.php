19cb2a9786caca76c254f3983df58894710acb32
tonmoy007 <fokiratonmoy@gmail.com>
2016-06-03 06:41:45 +0600

    Home Search

--- Fumontor/modules/Home/views/home-search-bar.php
+++ Fumontor/modules/Home/views/home-search-bar.php
@@ -1,5 +1,5 @@
 <div class="top-head">
-
+<link rel=stylesheet href="assets/css/home/switch.css">
 
     <div class="search-form-container cool-shadow" id="searchForm"  >
         <div class="form-overlay" >
@@ -20,22 +20,35 @@
                             <div class="row">
                                 <div class="col-xs-3 " ng-class="{active:type.checked}" ng-repeat="type in searchedFoodTypes">
                                     <label class="radio" ng-click="setActive(type.value)" ng-class="{active:type.checked}">
-                                        <input type="radio" name="" ng-model="foodtype.name" ng-value="type.value"><i class="col-xs-12 fa fa-diamond"></i><span>{{type.value}}</span>
+                                        <input type="radio" name="" ng-model="foodtype.name" ng-value="type.value">
+                                        <span><i class="fa fa-cutlery"></i></span>
+                                        <span>{{type.value}}</span>
                                     </label>
                                 </div>
                                 
                             </div>
                         </div>
                         <div class="tab-div "  >
+                        <div class="row search-switch">
+                            <div class="col-xs-12">
+                            <div class="search-order" ng-class="{active:!searchedOrderType}">
+                                Pre Order
+                            </div>
+                            <switch id="enabled" name="enabled" ng-model="searchedOrderType" on="" off="" class="red"></switch>
+                            <div class="search-order" ng-class="{active:searchedOrderType}">
+                                Instant Order
+                            </div>
+                        </div>
+                        </div>
                             <ul class="search-list ">
-                            <li class="form-list"><label>I am in </label></li>
+                            <li class="form-list"><label><i class="fa fa-location-arrow"></i></label></li>
 
-                                <li class="form-list"><select required="true" name="city" ng-init="query.city=cities[0].name" ng-model="query.city" ng-options="city.name for city in cities" class="form-control select select-primary select-block mbl">
+                                <!-- <li class="form-list"><select required="true" name="city" ng-init="query.city=cities[0].name" ng-model="query.city" ng-options="city.name for city in cities" class="form-control select select-primary select-block mbl">
                                 <option value="">City?</option>
                                     
-                                </select></li>
+                                </select></li> -->
                                            
-                                <li class="form-list"><input required="true" type="text"  placeholder="Area/Location" typeahead="c as c.name for c in places | filter:$viewValue | limitTo:10" typeahead-min-length='1' typeahead-on-select='onSelectPart($item, $model, $label)' ng-model="query.location" typeahead-template-url="home/getTamplate/searchTemplate" class="form-control" ></li>
+                                <li class="form-list big"><input required="true" type="text"  placeholder="Area/Location(Only In Dhaka)" typeahead="c as c.name for c in places | filter:$viewValue | limitTo:10" typeahead-min-length='1' typeahead-on-select='onSelectPart($item, $model, $label)' ng-model="query.location" typeahead-template-url="home/getTamplate/searchTemplate" class="form-control" ></li>
                            
 
                                 <li class="form-list"><button type="submit" class="btn btn-danger btn-emboshed btn-wide">Find ME FOOD </button></li>
